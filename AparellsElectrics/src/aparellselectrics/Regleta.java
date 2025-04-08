8b319217ea7b94f2c724998072d75e41f4afc05e
User <User@DESKTOP-LM6CDD5.mshome.net>
2025-04-08 17:58:26 +0200

    Añadido interface y clase enchufable.

---
+++ AparellsElectrics/src/aparellselectrics/Regleta.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package aparellselectrics;
+
+import java.util.HashSet;
+
+/**
+ *
+ * @author User
+ */
+public class Regleta {
+    private HashSet<Endollable> aparatos= new HashSet<Endollable>();
+    
+    
+    
+}
